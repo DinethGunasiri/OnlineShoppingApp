2be3106722d7764c35a06f227bc22771118d2eee
Dineth Lahiru <dineth.hewamadihage@student.curtin.edu.au>
2020-06-11 16:00:13 +0530

    Entity frame work installation

--- FrontEnd/ShoppingApp/src/app/app.module.ts
+++ FrontEnd/ShoppingApp/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import {MatCardModule} from '@angular/material/card';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
