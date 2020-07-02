201f4227d7bf872647dc57fe13df91d7fcb3fe32
Dineth Lahiru <dineth.hewamadihage@student.curtin.edu.au>
2020-07-02 17:21:18 +0530

    change user login

--- FrontEnd/OnlineShopping/src/app/login/login.component.ts
+++ FrontEnd/OnlineShopping/src/app/login/login.component.ts
@@ -15,6 +15,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   }
 }
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -22,8 +23,8 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class LoginComponent implements OnInit {
 
-  @Output() demo = new EventEmitter<string>();
-  
+ // @Output() demo = new EventEmitter<string>();
+
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
@@ -41,9 +42,12 @@ export class LoginComponent implements OnInit {
   dbPassword: any;
   passwordEncript: any;
   token: any = [];
-  customerDetails: any = [];
+  customerDetails: any;
   customerDetails2: any = [];
   isLoged: any = 'false';
+  customers: any = [];
+
+  status: any;
 
   constructor(private loginService: LoginServiceService,
               private customerSrvice: CustomerServiceService,
@@ -56,57 +60,64 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin(form: NgForm) {
-    
-    this.onGetCustomer(this.emailTxt, this.passwordTxt);
+ //   this. status = this.onGetCustomer(this.emailTxt, this.passwordTxt);
     this.onSetToken(this.emailTxt, this.passwordTxt);
-    this.onCheckCustomer();
   }
 
-  onGetCustomer(email: any, password: any) {
-    console.log(email);
+ /* onGetCustomer(email: any, password: any) {
+      this.customerSrvice.getCustomer(email).subscribe((data) => {
+        this.customerDetails = data;
+        console.log(this.customerDetails);
+        this.dbEmail = this.customerDetails.email;
+        this.dbPassword = this.customerDetails.password;
 
-    this.customerSrvice.getCustomer(email).subscribe((data = []) => {
-          this.customerDetails = data;
-
-          this.dbEmail = this.customerDetails.email;
-          this.dbPassword = this.customerDetails.password;
-
-          if (password == this.dbPassword && email == this.dbEmail)
-              {
-                this.isLoged = 'true';
-                this.cookieService.set('Loged', this.isLoged);
-                this.cookieService.set('fullName', this.customerDetails.fullName);
-                this.router.navigate(['products']);
-                this.notifyService.showSuccess(`Welcome ${this.customerDetails.fullName}`, 'Login Successfull');
-                this.dataService.callNavBar();
-              }
-              else {
-                this.notifyService.showError(`Invalid user name or password`, 'Login Error');
-                return;
-              }
-
-      });
-  }
+        if (password == this.dbPassword && email == this.dbEmail)
+            {
+              this.isLoged = 'true';
+              this.cookieService.set('Loged', this.isLoged);
+              this.cookieService.set('fullName', this.customerDetails.fullName);
+              this.router.navigate(['products']);
+              this.notifyService.showSuccess(`Welcome ${this.customerDetails.fullName}`, 'Login Successfull');
+              this.dataService.callNavBar();
+            }
+            else {
+              this.notifyService.showError(`Invalid user name or password`, 'Login Error');
+            }
+    }, error => {
+      console.log(error);
+      this.notifyService.showError(`Invalid user name or password`, 'Login Error');
+    });
+      console.log('test');
+    // this.onCheckCustomer(this.customerDetails);
+  } */
 
   onSetToken(email: any, password: any) {
     this.loginService.loginCustomer(email, password).subscribe((data: []) => {
         this.token = data;
         this.cookieService.set('Token', this.token.token.result);
-       // sessionStorage.setItem('Token', this.token.token.result);
 
         if (this.token.token.result != null) {
+
+          this.customerSrvice.getCustomer(email).subscribe((data2: []) => {
+            this.customerDetails = data;
+
+            this.isLoged = 'true';
+            this.cookieService.set('Loged', this.isLoged);
+            this.cookieService.set('fullName', this.customerDetails.fullName);
+            this.router.navigate(['products']);
+            this.notifyService.showSuccess(`Welcome ${this.customerDetails.fullName}`, 'Login Successfull');
+            this.dataService.callNavBar();
+          });
+
           this.customerSrvice.getCustomers().subscribe((data2: []) => {
             this.customerDetails2 = data2;
+            console.log(this.customerDetails2);
           });
         }
+      }, error => {
+        console.log(error);
+        this.notifyService.showError(`Invalid user name or password`, 'Login Error');
       });
-   }
-
-   onCheckCustomer() {
-    if (this.customerDetails.length == 0) {
-      this.notifyService.showError(`Invalid user name`, 'Login Error');
-      return;
-    }
    }
 
 }
